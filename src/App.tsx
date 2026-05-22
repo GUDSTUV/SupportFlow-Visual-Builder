985a52698cc806e636f8c20944d7171eb11c51f9
Your Name <you@example.com>
2026-05-22 01:16:53 +0000

    Initialize React TypeScript project with Vite

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+
+function App() {
+
+  return (
+     <div className="h-screen flex items-center justify-center bg-blue-500 text-white text-2xl">
+      Tailwind v4 is working
+    </div>
+  )
+}
+
+export default App
